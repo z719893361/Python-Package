a571a4756bd97c518caeeff56ab84ea5446fb786
z719893361 <38043183+z719893361@users.noreply.github.com>
2021-01-10 17:13:20 +0800

    Update threadpool.py

--- thread/threadpool.py
+++ thread/threadpool.py
@@ -5,6 +5,10 @@ import threading
 
 class ThreadPool:
     def __init__(self, thread_max_num, max_task_num=0):
+        """
+        :param thread_max_num:  最大线程数
+        :param max_task_num:    最大队列数
+        """
         # max_num      线程池最大线程数量
         # max_task_num 任务队列长度
         self.queue = queue.Queue(max_task_num)
@@ -20,18 +24,24 @@ class ThreadPool:
         self.free_list = []
 
     def put(self, func, kwargs=None, callback=None):   # 往任务队列加入一个任务
+        """
+        :param func:        方法对象
+        :param kwargs:      传入参数
+        :param callback:    回调函数(success, result)
+        :return:
+        """
         # 判断任务是否已经取消
         if self.cancel:
             return
         # 如果没有空闲的线程, 并且已经创建的线程数量小于预设的线程数量, 则创建新线程
         if len(self.free_list) == 0 and len(self.generate_list) < self.thread_max_num:
-            threading.Thread(target=self.call).start()
+            threading.Thread(target=self.__call).start()
         # 构造任务元组, 分别是 方法, 参数, 回调方法
         w = (func, kwargs, callback)
         # 将任务添加到任务队列
         self.queue.put(w)
 
-    def call(self):
+    def __call(self):
         # 获取当前线程名
         current_thread = threading.currentThread().getName()
         # 获取线程名, 将线程名加入到已经实例化线程列表中
@@ -61,7 +71,7 @@ class ThreadPool:
                     callback(success, result)
                 except Exception as error:
                     print(str('执行回调函数出错 %s' % error))
-            with self.worker_state(self.free_list, current_thread):
+            with self.__worker_state(self.free_list, current_thread):
                 if self.interrupt:
                     # event等于None 跳出循环
                     event = None
@@ -73,19 +83,26 @@ class ThreadPool:
             self.generate_list.remove(current_thread)
 
     def close(self):
+        """
+        不在添加任务, 执行完成即退出
+        """
         self.cancel = True
         # 计算已创建的线程个数, 然后往任务队列里推送数量相同的标识元素
         while self.generate_list:
             self.queue.put(None)
 
     def quit(self):
+        """
+        强制退出
+        """
         self.interrupt = True
         # 计算已创建的线程个数, 然后往任务队列里推送数量相同的标识元素
         while self.generate_list:
             self.queue.put(None)
 
+    # 上下文管理,放入
     @contextlib.contextmanager
-    def worker_state(self, state_list, worker_thread):
+    def __worker_state(self, state_list, worker_thread):
         state_list.append(worker_thread)
         try:
             yield
@@ -95,7 +112,13 @@ class ThreadPool:
             state_list.remove(worker_thread)
 
     def get_free_num(self):
+        """
+        :return:    空闲线程数量
+        """
         return len(self.free_list)
 
     def get_generate_num(self):
+        """
+        :return:    获取实例化线程数
+        """
         return len(self.generate_list)
